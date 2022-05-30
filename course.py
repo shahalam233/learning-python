88aa571061b1702bf1d895108880c7020d84d27f
Shah Alam <onlyshagor1@gmail.com>
2022-05-30 23:13:24 +0600

    30may-last

--- course.py
+++ course.py
@@ -1,7 +1,54 @@
 
-first = "shah"
-last = "alam"
-full = first + " " + last
-print(full)
-same = f"{print(first)} {last}"
-print(same)
+#first = "shah"
+#last = "alam"
+#full = first + " " + last
+# print(full)
+#same = f"{print(first)} {last}"
+# print(same)
+#import math
+#from math import ceil
+#from re import X
+
+
+#class1 = "  math maTics"
+# print(class1)
+# print(class1.upper())
+# print(class1.lower())
+# print(class1.title())
+# print(class1.strip())
+# print(class1.find("th"))
+#print(class1.replace("m", "S"))
+# print(class1.find("m"))
+#print("ma" in class1)
+#print("ma" not in class1)
+
+#print(20 + 3)
+#print(20 - 3)
+#print(20 * 3)
+#print(20 / 3)
+# print(20/3)
+#print(20 ** 3)
+#print(20 // 3)
+#print(20 % 3)
+#x = 10
+#x /= x
+# print(x)
+# print(round(2.4))
+# print(round(2.6))
+# print(abs(-6.9))
+# print(ceil(3.1))
+# print(math.degrees(5))
+
+#x = input("x: ")
+# print(type(x))
+#y = int(x) + 1
+#print(f"x: {x}, y: {y}")
+# print(bool(0))
+# print(bool(9))
+# print(bool(""))
+# print(bool("Flase"))
+fruit = "Mango fruit"
+print(fruit[1])
+print(fruit[1:-0])
+print(fruit[0:10])
+print(20 % 4)
